5de5ba194af2cc3569f6310d50e9f0426fe258aa
WilD <a.nazarenko@nylonfabric.ru>
2021-08-26 21:23:25 +0300

    add routes and pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, {Component} from 'react'
 import Layout from './hoc/Layout/Layout'
+import { Route, Switch } from 'react-router-dom'
 import Quiz from './Containers/Quiz/Quiz'
+import QuizList from './Containers/QuizList/QuizList'
+import Auth from './Containers/Auth/Auth'
+import QuizCreator from './Containers/QuizCreator/QuizCreator'
 
 class App extends Component {
   render() {
     return (
       <Layout>
-        <Quiz />
+        <Switch>
+          <Route path={'/auth'} component={Auth}/>
+          <Route path={'/quiz-creator'} component={QuizCreator}/>
+          <Route path={'/quiz/:id'} component={Quiz}/>
+          <Route path={'/'} component={QuizList}/>
+
+        </Switch>
       </Layout>
     )
   }
